6b1e67548b169f0986be1eca8e151667c6195137
bszydelko <szydelkob@hotmail.com>
2020-05-02 23:51:30 +0200

    Read file

--- TS_parser.cpp
+++ TS_parser.cpp
@@ -1,19 +1,25 @@
 #include "tsCommon.h"
 #include "tsTransportStream.h"
 
+
 int main( int argc, char *argv[ ], char *envp[ ])
 {
-  // TODO - open file
+    const char* filename = "../example_new.ts";
+    FILE* TS_fileHandler = fopen(filename, "rb");
+    if (!TS_fileHandler)
+        return 0;
 
+    uint8_t* TS_PacketBuffer = new uint8_t[xTS::TS_PacketLength];
 
   xTS_PacketHeader    TS_PacketHeader;
 
   int32_t TS_PacketId = 0;
-  while(/*not eof*/)
+  while(!feof(TS_fileHandler))
   {
-    // TODO - read from file
 
-    TS_PacketHeader.Reset();
+      TS_PacketHeader.Reset();
+      fread(TS_PacketBuffer, xTS::TS_PacketLength, 1, TS_fileHandler);
+
     TS_PacketHeader.Parse(TS_PacketBuffer);
 
     printf("%010d ", TS_PacketId);
--- tsTransportStream.cpp
+++ tsTransportStream.cpp
@@ -27,39 +27,38 @@ void xTS_PacketHeader::Reset()
 	CC .reset();
 }
 
+std::string bits(const uint8_t* _Input, size_t _startByte, size_t _count)
+{
+	std::string c_bin = "";
+	std::string s_bin = "";
+	int bit = 0;
+	int c = 0;
+
+	for (size_t byte = _startByte; byte < _count; byte++)
+	{
+		c_bin = "";
+		c = int(_Input[byte]);
+
+		bit = 0;
+		while (bit < CHAR_BIT)
+		{
+			(c % 2) ? c_bin.push_back('1') : c_bin.push_back('0');
+			c >>= 1;
+			bit++;
+		}
+		std::reverse(c_bin.begin(), c_bin.end());
+		s_bin.append(c_bin);
+	}
+
+	return s_bin;
+};
+
 int32_t xTS_PacketHeader::Parse(const uint8_t* Input)
 {
-	auto bits = [](const uint8_t* _Input, size_t _count)
-	{
-		std::string c_bin = "";
-		std::string s_bin = "";
-		int bit = 0;
-		int c = 0;
+	std::string InputBin = bits(Input, 0, 4);
+	std::istringstream bit_stream(InputBin);
 
-		for (size_t byte = 0; byte < _count; byte++)
-		{
-			c_bin = "";
-			c = int(_Input[byte]);
-
-
-			while (bit < CHAR_BIT)
-			{
-				(c % 2) ? c_bin.push_back('1'): c_bin.push_back('0');
-				c >>= 2;
-				bit++;
-			}
-			std::reverse(c_bin.begin(), c_bin.end());
-			s_bin.append(c_bin);
-		}
-
-		return s_bin;
-	};
-
-	std::string InputBin = bits(Input, 4);
-
-	std::istringstream raw_data(InputBin);
-
-	raw_data >> SB >> E >> S >> P >> PID >> TSC >> AFC >> CC;
+	bit_stream >> SB >> E >> S >> P >> PID >> TSC >> AFC >> CC;
 
 	return int32_t();
 }
@@ -71,13 +70,13 @@ void xTS_PacketHeader::Print() const
 	oss <<
 		"TS:" <<
 		" SB=" << std::to_string(SB.to_ulong()) <<
-		" E=" << std::to_string(SB.to_ulong()) <<
-		" S=" << std::to_string(SB.to_ulong()) <<
-		" P=" << std::to_string(SB.to_ulong()) <<
-		" PID=" << std::setw(4) << std::to_string(SB.to_ulong()) <<
-		" TSC=" << std::to_string(SB.to_ulong()) <<
-		" AFC=" << std::to_string(SB.to_ulong()) <<
-		" CC=" << std::setw(2) << std::to_string(SB.to_ulong());
+		" E=" << std::to_string(E.to_ulong()) <<
+		" S=" << std::to_string(S.to_ulong()) <<
+		" P=" << std::to_string(P.to_ulong()) <<
+		" PID=" << std::setw(4) << std::to_string(PID.to_ulong()) <<
+		" TSC=" << std::to_string(TSC.to_ulong()) <<
+		" AFC=" << std::to_string(AFC.to_ulong()) <<
+		" CC=" << std::setw(2) << std::to_string(CC.to_ulong());
 
 	std::cout << oss.str();
 
